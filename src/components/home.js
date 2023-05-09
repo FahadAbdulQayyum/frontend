eb9f9b1c4b786aafd336858a0f56940fb6493fde
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-05-09 07:53:41 +0500

    check comment

--- src/components/home.js
+++ src/components/home.js
@@ -38,8 +38,13 @@ const Home = () => {
         console.log('useEffect()', task)
         let auth = JSON.parse(localStorage.getItem('IsAuthenticated'))
         console.log('authhh, isAuth', auth, isAuthenticated)
+        console.log('auth', auth)
+        console.log('!auth', !auth)
         if (auth) {
             setIsAuthenticated(true)
+        }
+        else if (!auth) {
+            setIsAuthenticated(false)
         }
         if (!isAuthenticated) return navigate('/login')
     }, [])
